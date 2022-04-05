17c9c961ad10e6fe6fbf75bb0a0f5e0feefaa544
Andrew Lounsbury <lounsburya@gmail.com>
2022-04-05 08:53:14 -0500

    couple more done

--- joins-solution.sql
+++ joins-solution.sql
@@ -19,5 +19,10 @@ JOIN "products" ON "products"."id"="warehouse"."id";
 WHERE "products"."description" = 'diet pepsi';
 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 6. How many customers do we have?
+SELECT count(*) FROM "customers";
 7. How many products do we carry?
+SELECT count(*) FROM "products"
 8. What is the total available on-hand quantity of diet pepsi?
+SELECT count(*) FROM "wharehouse_products"
+JOIN "products" ON "products"."id"="wharehouse_products"."product_id"
+WHERE "products"."description"='diet pepsi';
